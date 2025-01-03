6698790c7b402e4319b755ccc5bce6ca6a95ad05
Kevin Kipper <kevinkippervfx@socal.rr.com>
2025-01-03 11:02:32 -0800

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # tg-go-to-frame
 Sets the current frame in the Terragen project.
+
+![tg_go_to_frame UI](images/tg_goto_frame_GUI.jpg)
+
+### Requirements
+Terragen 4 Professional v4.6.31 (or later) <br>
+https://planetside.co.uk/
+
+### Installation
+Install Terragen 4 Professional v4.6.31 or greater on your computer.
+
+In this repository you’ll find two Python scripts, which are identical except for their file extensions.  The file ending in .PY will open a command window when run, while the file ending in .PYW will not.  I recommend using the file with the .PYW extension when the script is run or called from an external file or controller device like a Tourbox.
+
+### Usage
+When run, the UI presents a single Entry widget called “<b>Go to frame</b>” which accepts integer values. The default value is the current frame, and this value can be retrieved at any time by pressing the <b>Escape key</b>.   The current frame and timeline will automatically update as characters are entered.  The <b>Up Arrow</b> and <b>Down Arrow</b> will advance the current frame forward or backwards respectively by one frame.  Scrolling the <b>middle mouse button</b> will advance the “Go to frame” value, but you must click another key, such as the <b>Enter</b> key, to set the current frame and update the timeline.
+
+### Reference
+terragen-rpc <br>
+https://github.com/planetside-software/terragen-rpc
+
+Online documentation for Terragen RPC <br>
+https://planetside.co.uk/docs/terragen-rpc/
+
+Blog posts on using Terragen RPC <br>
+https://planetside.co.uk/blog/hello-rpc-part-1-scripting-for-terragen/ <br>
+https://planetside.co.uk/blog/hello-rpc-part-2-error-handling/ <br>
+https://planetside.co.uk/blog/script-it-yourself-kelvin-sunlight-colour-with-terragen-rpc/
+
---
+++ tg_go_to_frame.py
@@ -0,0 +1,215 @@
+'''
+tg_go_to_frame.py - Sets the current frame in the Terragen project.
+
+When first executed the Entry widget value is set to the current frame
+in the project.  
+
+Certain keyboard bindings can be used as shortcuts to modify the value
+of the Entry widget.
+    Up arrow - step to next frame
+    Down arrow - step to previous frame
+    Escape - resets Entry widget to initial value
+    MouseWheel up - steps to next frames (must press Enter key to accept)
+    MouseWheel down -steps to previous frames (must press Enter key to accept)
+'''
+
+import os.path
+import traceback
+import tkinter as tk
+from tkinter import messagebox
+import terragen_rpc as tg
+
+gui = tk.Tk()
+gui.title(os.path.basename(__file__))
+gui.geometry("280x50")
+
+def popup_warning(title, message) -> None:
+    '''
+    Opens a window and displays a message.
+
+    Args:
+        title (str): Characters displayed at the top of the window.
+        message (str): Characters displayed in the body of the window.
+
+    Returns:
+        None
+    '''
+    messagebox.showwarning(title, message)
+
+def is_valid_integer(value):
+    """
+    Check if the string value is a valid integer.
+
+    Args:
+        value (str): Current value of the Entry widget
+
+    Returns:
+        (bool)
+    """
+    global ERROR_SHOWN
+    try:
+        int(value)
+        return True
+    except ValueError:
+        if not ERROR_SHOWN:
+            popup_warning("Invalid input", "Please enter a valid integer.")
+            ERROR_SHOWN = True
+        return False
+
+def on_go_to(_) -> None:
+    '''
+    Advances the current frame according to the value in the Entry widget.
+
+    Args:
+        (_): Unused keyrelease event
+
+    Returns:
+        None
+    '''
+    global ERROR_SHOWN
+    current_frame = current_frame_var.get()
+    if is_valid_integer(current_frame):
+        ERROR_SHOWN = False
+        try:
+            project = tg.root()
+            project.set_param("current_frame", int(current_frame_var.get()))
+        except ConnectionError as e:
+            popup_warning(os.path.basename(__file__), "Terragen RPC connection error" + str(e))
+            return None
+        except TimeoutError as e:
+            popup_warning(os.path.basename(__file__), "Terragen RPC timeout error" + str(e))
+            return None
+        except tg.ReplyError as e:
+            popup_warning(os.path.basename(__file__), "Terragen RPC reply error" + str(e))
+            return None
+        except tg.ApiError:
+            popup_warning(
+                os.path.basename(__file__),
+                "Terragen RPC API error" + str(traceback.format_exc())
+                )
+            return None
+
+def decrease_value(_) -> None:
+    '''
+    Decreases the current frame value by one.
+
+    Args:
+        (_): Unused down arrow event
+
+    Returns:
+        None
+    '''
+    current_frame = current_frame_var.get()
+    if is_valid_integer(current_frame):
+        current_frame_var.set(int(current_frame) - 1)
+
+def increase_value(_) -> None:
+    '''
+    Increases the current frame value by one.
+
+    Args:
+        (_): Unused up arrow event
+
+    Returns:
+        None
+    '''
+    current_frame = current_frame_var.get()
+    if is_valid_integer(current_frame):
+        current_frame_var.set(int(current_frame) + 1)
+
+def on_mouse_wheel(event) -> None:
+    '''
+    Increases or decreases the current frame value.
+
+    Args:
+        (event): Mousewheel up or down scrolling
+
+    Returns:
+        None
+    '''
+    current_value = current_frame_var.get()
+    if is_valid_integer(current_value):
+        if event.delta > 0:
+            current_frame_var.set(str(int(current_value) + 1))
+        elif event.delta < 0:
+            current_frame_var.set(str(int(current_value) - 1))
+    else:
+        print("Invalid input")
+
+def on_escape(_) -> None:
+    '''
+    Resets the Entry widget value to its original value at the time the
+    script was first run.
+
+    Args:
+        (_): Unused escape key event
+
+    Returns:
+        None
+    '''
+    current_frame_var.set(current_frame_at_startup.get())
+
+def on_startup() -> None:
+    '''
+    Gets initial values from the current Terragen projects and sets the
+    global tkinter variables.
+
+    Returns:
+        None
+    '''
+    try:
+        project = tg.root()
+        current_frame_at_startup.set(project.get_param("current_frame"))
+        current_frame_var.set(current_frame_at_startup.get())
+        start_frame_var.set(project.get_param("start_frame"))
+        end_frame_var.set(project.get_param("end_frame"))
+    except ConnectionError as e:
+        popup_warning(os.path.basename(__file__), "Terragen RPC connection error" + str(e))
+        return None
+    except TimeoutError as e:
+        popup_warning(os.path.basename(__file__), "Terragen RPC timeout error" + str(e))
+        return None
+    except tg.ReplyError as e:
+        popup_warning(os.path.basename(__file__), "Terragen RPC reply error" + str(e))
+        return None
+    except tg.ApiError:
+        popup_warning(
+            os.path.basename(__file__),
+            "Terragen RPC API error" + str(traceback.format_exc())
+            )
+        return None
+
+def set_focus_and_select() -> None:
+    '''
+    Sets focus to the entry widget and select the entire text
+
+    Returns:
+        None
+    '''
+    frame.focus_set()
+    frame.select_range(0, tk.END)
+
+# tkinker variables
+current_frame_at_startup = tk.StringVar()
+current_frame_var = tk.StringVar()
+start_frame_var = tk.StringVar()
+end_frame_var = tk.StringVar()
+ERROR_SHOWN = False
+
+# main
+on_startup()
+
+# gui
+tk.Label(gui, text="Go to frame: ").grid(row=0, column=0, padx=4, pady=10, sticky="w")
+
+frame = tk.Entry(gui, textvariable=current_frame_var, width=10)
+frame.grid(row=0, column=1, padx=4, pady=10, sticky="w")
+frame.bind("<MouseWheel>", on_mouse_wheel)
+frame.bind("<Up>", increase_value)
+frame.bind("<Down>", decrease_value)
+frame.bind("<KeyRelease>", on_go_to)
+frame.bind("<Escape>", on_escape)
+
+set_focus_and_select()
+
+gui.mainloop()
